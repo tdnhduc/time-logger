8b669d1487722d75c7017ac1e3cda430a394b22d
DucNguyen <ducnh@inspirelab.io>
2022-04-19 23:23:08 +0700

    init repo

---
+++ http-server/src/main/java/com/ductd/server/Runner.java
@@ -0,0 +1,18 @@
+/*
+ * @created 19/04/2022 - 23:16
+ * @author Duc Nguyen
+ *
+ */
+
+package com.ductd.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Runner {
+    private static final Logger log = LoggerFactory.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+        log.info("hello world");
+    }
+}
